00a102eaacf84aba40ed018c05697fad6e0abaae
MarSurget <martinaaylen.surgetalbano@estudiantes.unahur.edu.ar>
2026-07-01 11:22:21 -0300

    logo en footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,31 @@
+import { useTheme } from "../context/ModeContext";
+import logoClaro from "../assets/logo claro.png";
+import logoOscuro from "../assets/logo oscuro.png";
+
 function Footer() {
+  const { tema } = useTheme();
+
+  const logo = tema === "light"
+    ? logoOscuro
+    : logoClaro;
   return (
-    <footer className="bg-gray-900 text-gray-300 py-5">
-      <div className="max-w-7xl mx-auto px-6 text-center">
-        <h2 className="text-xl font-bold text-white mb-2">
+    <footer className="bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 py-6">
+      <div className="max-w-6xl mx-auto px-4 flex flex-col items-center gap-3">
+    <img
+      src={logo}
+      alt="UNaHur Anti-Social Net"
+      className="w-16 h-16 object-contain"/>
+
+        <h2 className="text-sm font-bold text-gray-600 dark:text-gray-300 text-center">
           UNaHur Anti-Social Net
         </h2>
 
-        <p className="text-sm mb-4">
+        <p className="text-sm mb-4 text-gray-600 dark:text-gray-300">
           Una red social para compartir publicaciones, interactuar y conectar
           con la comunidad.
         </p>
 
-        <div className="text-xs text-gray-500 space-y-1">
+        <div className="text-xs text-gray-600 dark:text-gray-300 space-y-1">
           <p>© 2026 UNaHur Anti-Social Net - Todos los derechos reservados</p>
         </div>
       </div>
